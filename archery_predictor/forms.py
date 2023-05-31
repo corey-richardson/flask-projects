f8fbb2d6abdbd7073450774d95e4f1996af6ddd2
Corey Richardson <richardsoncorey04@gmail.com>
2023-05-31 10:24:59 +0000

    Post competition update

---
+++ archery_predictor/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import RadioField, SubmitField, IntegerField, BooleanField
+from wtforms.validators import DataRequired
+
+class GetScoreData(FlaskForm):
+    distance = RadioField(
+        "Distance",
+        choices = [
+            (10.0, "10 yards"),
+            (19.685, "18 metres"),
+            (20.0, "20 yards"),
+            (30.0, "30 yards"),
+            (40.0, "40 yards"),
+            (50.0, "50 yards"),
+            (54.6807, "50 metres"),
+            (60, "60 yards"),
+            (76.5529, "70 metres"),
+            (80.0, "80 yards"),
+            (100.0, "100 yards")
+        ]
+    )
+    days_till = IntegerField("Days Till Shoot: ", validators=[DataRequired()])
+    is_comp = BooleanField("Competition? ")
+    submit = SubmitField("Submit")
