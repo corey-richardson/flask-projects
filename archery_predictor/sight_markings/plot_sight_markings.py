f8d12c981d9fb7870ddd0544fd98acdd8bc30a7b
Corey Richardson <richardsoncorey04@gmail.com>
2023-07-04 08:01:17 +0000

    sight markings

--- archery_predictor/sight_markings/plot_sight_markings.py
+++ archery_predictor/sight_markings/plot_sight_markings.py
@@ -10,15 +10,14 @@ distance_to_predict = 80
 is_metres = 0
 
 # Function to convert metres to yards
-def convert_to_yards(distance, is_metres):
-    if not is_metres: # if yards, no conversion necessary
-        return distance
+def convert_to_yards(distance):
     return distance * 1.09361
 
-distance_to_predict = convert_to_yards(distance_to_predict, is_metres)
+if is_metres:
+    distance_to_predict = convert_to_yards(distance_to_predict)
 
 # Read data from csv
-scope = pd.read_csv(f"sight_marks.csv", header = 0)
+scope = pd.read_csv(f"sight_markings/sight_marks.csv", header = 0)
 distances = scope["distance"]
 sight_markings = scope["sight_marking"]
 
@@ -43,7 +42,13 @@ distance_predicted = model.predict(distance_to_predict)
 # Plot
 plt.scatter(distances, sight_markings, color='k', marker='x')
 plt.plot(ranging, predicted_sight_markings)
-plt.axhline(distance_predicted, color="k", linestyle="dashed")
+plt.axhline(distance_predicted, color="r", linestyle="dashed")
+plt.axhline(0, color="k", linestyle=":", alpha=0.5)
+plt.axhline(6, color="k", linestyle=":", alpha=0.5)
+
+print(f"At 0yds: {model.intercept_[0]:.2f}")
+print(f"Delta per 10yds: {model.coef_[0][0] * 10:.2f}")
+print(f"At {distance_to_predict[0][0]:.2f}yds: {distance_predicted[0][0]:.2f}")
 
 plt.title(f"{distance_predicted[0][0]:.2f} @ {distance_to_predict[0][0]:.2f} yards")
-plt.savefig("sight_markings.png")
+plt.savefig("sight_markings/sight_markings.png")
